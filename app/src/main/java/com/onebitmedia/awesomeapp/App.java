835a1bd91a035b5491c997037e77ff96f603e5f0
Akhyar Amarullah <akhyrul@gmail.com>
2015-06-24 19:03:05 +0700

    Make bare template

--- app/src/main/java/com/onebitmedia/awesomeapp/App.java
+++ app/src/main/java/com/onebitmedia/awesomeapp/App.java
@@ -5,9 +5,6 @@ import android.content.Context;
 
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.picasso.OkHttpDownloader;
-import com.squareup.picasso.Picasso;
 
 import timber.log.Timber;
 
@@ -42,15 +39,12 @@ public class App extends Application {
         Timber.plant(BuildConfig.DEBUG ? new Timber.DebugTree() : new Timber.Tree() {
             @Override
             protected void log(int priority, String tag, String message, Throwable t) {
-                //TODO [template] add remote logger code here (e.g. Crashlytics)
+                //TODO [template] add remote logger initialization (e.g. Crashlytics)
             }
         });
     }
 
     private void initImageLoader() {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        Picasso.setSingletonInstance(new Picasso.Builder(this)
-            .downloader(new OkHttpDownloader(okHttpClient))
-            .build());
+        //TODO [template] add image loader initialization (e.g. Picasso, Glide, etc)
     }
 }
